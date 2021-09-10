6757da38b3be18e71f4015dded3413c25d4596b9
Jascha030 <contact@jaschavanaalst.nl>
2021-09-10 10:28:04 +0200

    init lua

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,7 +1,7 @@
 local fn = vim.fn
 local install_path = fn.stdpath('data') .. '/site/pack/packer/opt/packer.nvim'
-
 local execute = vim.api.nvim_command
+
 if fn.empty(fn.glob(install_path)) > 0 then
     fn.system({ 'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path })
     execute 'packadd packer.nvim'
